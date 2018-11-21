bf3a1ce735b589bde6f327ab97eafa2fc56311a8
losfair <zhy20000919@hotmail.com>
2018-11-21 18:51:52 +0800

    xltypeck: Use parser

--- src/bin/xltypeck.rs
+++ src/bin/xltypeck.rs
@@ -13,16 +13,18 @@ fn main() {
         .unwrap()
         .read_to_string(&mut ast_text)
         .unwrap();
+    {
+        let mut ast: x_lang::ast::Expr = x_lang::parser::parse_expr(ast_text.as_str()).unwrap();
+        println!("{}", serde_yaml::to_string(&ast).unwrap());
 
-    let mut ast: x_lang::ast::Expr = serde_yaml::from_str(&ast_text).unwrap();
+        let mut trs = x_lang::typeck::TypeResolveState::default();
 
-    let mut trs = x_lang::typeck::TypeResolveState::default();
+        let hm = x_lang::corelib::HostManager::new();
+        trs.add_hosts(hm.get_binops());
+        trs.add_hosts(hm.get_ifop());
+        trs.add_hosts(hm.get_relops());
 
-    let hm = x_lang::corelib::HostManager::new();
-    trs.add_hosts(hm.get_binops());
-    trs.add_hosts(hm.get_ifop());
-    trs.add_hosts(hm.get_relops());
-
-    let ty = x_lang::typeck::check_expr(&mut ast, &mut trs).unwrap();
-    println!("{:?}", ty);
+        let ty = x_lang::typeck::check_expr(&mut ast, &mut trs).unwrap();
+        println!("{:?}", ty);
+    }
 }
